5b7cd54b52bed4e4ebd5d2e50057990d43fa6238
emredalci <emredalc@gmail.com>
2023-01-28 20:32:11 +0300

    Added config-server

--- library-service/src/main/java/com/store/book/libraryservice/controller/LibraryController.java
+++ library-service/src/main/java/com/store/book/libraryservice/controller/LibraryController.java
@@ -5,6 +5,7 @@ import com.store.book.libraryservice.dto.LibraryDto;
 import com.store.book.libraryservice.service.LibraryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,9 @@ public class LibraryController {
     private final LibraryService libraryService;
 
     private final Environment environment;
+
+    @Value("${library.service.count}")
+    private String count;
 
     public LibraryController(LibraryService libraryService, Environment environment) {
         this.libraryService = libraryService;
@@ -46,5 +50,10 @@ public class LibraryController {
     @GetMapping
     public ResponseEntity<List<String>> getAllLibraries() {
         return ResponseEntity.ok(libraryService.getAllLibraries());
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount() {
+        return ResponseEntity.ok("Library count is : " + count);
     }
 }
